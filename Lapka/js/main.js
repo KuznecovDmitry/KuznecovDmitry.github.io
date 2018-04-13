76846d9f20529ee8d7fa6a7dabc6a8a99778ef05
KuznecovDmitry <overburdened.address@gmail.com>
2018-04-13 10:51:22 +0300

    add project

--- Lapka/js/main.js
+++ Lapka/js/main.js
@@ -36,25 +36,24 @@
     // Offset for Main Navigation
     $('#mainNav').affix({
         offset: {
-            top: 800
+            top: 500
         }
     })
-
 
     // Map
     var map = new google.maps.Map(document.getElementById('google-container'), {
         zoom: 17,
-        center: new google.maps.LatLng(55.833323, 37.508801),
+        center: new google.maps.LatLng(46.482526, 30.723395),
         scrollwheel:  false,
         panControl: false,
         zoomControl: true,
         mapTypeControl: false,
         streetViewControl: false,
-        styles: [{"featureType":"water","elementType":"all","stylers":[{"hue":"#15a4aa"},{"saturation":60},{"lightness":-51},{"visibility":"on"}]},{"featureType":"administrative","elementType":"all","stylers":[{"hue":"#15a4aa"},{"saturation":41},{"lightness":-66},{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#f48141"},{"saturation":10},{"lightness":-20},{"visibility":"on"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"on"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"hue":"#f48141"},{"saturation":87},{"lightness":-20},{"visibility":"simplified"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"hue":"#f48141"},{"saturation":81},{"lightness":-22},{"visibility":"on"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"hue":"#f48141"},{"saturation":79},{"lightness":-27},{"visibility":"simplified"}]},{"featureType":"administrative.neighborhood","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":0},{"lightness":100},{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":0},{"lightness":100},{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":0},{"lightness":100},{"visibility":"off"}]},{"featureType":"administrative","elementType":"all","stylers":[{"hue":"#43DAE0"},{"saturation":78},{"lightness":-27},{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":0},{"lightness":100},{"visibility":"off"}]}]
+        styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":-5}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"lightness":30}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ef8c25"},{"lightness":40}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#b6c54c"},{"lightness":40},{"saturation":-40}]},{}]
     });
 
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(55.833323, 37.508801),
+        position: new google.maps.LatLng(46.482526, 30.723395),
         map: map,
         icon: 'img/location.png'
     });
